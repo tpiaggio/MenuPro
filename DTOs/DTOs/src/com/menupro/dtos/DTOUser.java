d5aedf38b4ead7e2183f52aaa84b8b686050ec8e
jdelprato257 <jdelpratola@gmail.com>
2015-06-22 20:50:49 -0300

    DTOs

--- DTOs/DTOs/src/com/menupro/dtos/DTOUser.java
+++ DTOs/DTOs/src/com/menupro/dtos/DTOUser.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dtos;
+package com.menupro.dtos;
 
 import java.util.ArrayList;
 import java.util.List;
