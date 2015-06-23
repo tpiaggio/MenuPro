c2445b42f69d5c9f9d667c2344e7c55e99c864d8
jdelprato257 <jdelpratola@gmail.com>
2015-06-22 22:18:25 -0300

    Se termino DTO a Entity

--- MenuPro-ejb/src/java/com/menupro/business/transformers/DtoToEntityTransformer.java
+++ MenuPro-ejb/src/java/com/menupro/business/transformers/DtoToEntityTransformer.java
@@ -19,27 +19,71 @@ public class DtoToEntityTransformer implements DtoToEntityTransformerLocal {
 
     @Override
     public User convertUser(DTOUser dUser) {
-        return null;
+        User newUser=new User();
+        newUser.setId(dUser.getId());
+        newUser.setName(dUser.getName());
+        newUser.setPassword(dUser.getPassword());
+        newUser.setUserName(dUser.getUserName());
+        for(DTOUser dU : dUser.getContacts()){
+            User listUser=new User(dU.getId(),dU.getUserName(),
+                dU.getName(), dU.getPassword());
+            newUser.getContacts().add(listUser);
+        }
+        return newUser;
     }
-    
-    
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
-
+    public User converNormalUser(DTOUser dUser){
+        User newUser=new User();
+        newUser.setId(dUser.getId());
+        newUser.setName(dUser.getName());
+        newUser.setPassword(dUser.getPassword());
+        newUser.setUserName(dUser.getUserName());
+        return newUser;
+    }
     @Override
     public Order convertOrder(DTOOrder dOrder) {
+        Order newOrder = new Order();
+        newOrder.setComments(dOrder.getComments());
+        newOrder.setDate(dOrder.getDate());
+        newOrder.setId(dOrder.getId());
+        User newUser = this.converNormalUser(dOrder.getBuyer());
+        newOrder.setBuyer(newUser);
+        for(DTOUser dU : dOrder.getSideBuyers()){
+            User sU = this.converNormalUser(dU);
+            newOrder.getSideBuyers().add(sU);
+        }
+        for(DTOMenu dM : dOrder.getMenus()){
+            Menu rM = this.convertMenu(dM);
+            newOrder.getMenus().add(rM);
+        }
         return null;
     }
-
+    
     @Override
     public Menu convertMenu(DTOMenu dMenu) {
+        Menu newMenu = new Menu();
+        newMenu.setId(dMenu.getId());
+        newMenu.setName(dMenu.getName());
+        User newUser = this.converNormalUser(dMenu.getOwner());
+        newMenu.setOwner(newUser);
+        for(DTOUser dU : dMenu.getSharedUsers()){
+            User sU = this.converNormalUser(dU);
+            newMenu.getSharedUsers().add(sU);
+        }
+        for(DTOPlate dP : dMenu.getPlates()){
+            Plate rP = this.convertPlate(dP);
+            newMenu.getPlates().add(rP);
+        }
         return null;
     }
 
     @Override
     public Plate convertPlate(DTOPlate dPlate) {
-        return null;
+        Plate newPlate = new Plate();
+        newPlate.setId(dPlate.getId());
+        newPlate.setCategory(dPlate.getCategory());
+        newPlate.setName(dPlate.getName());
+        newPlate.setPrice(dPlate.getPrice());    
+        return newPlate;
     }
 
-    
 }
